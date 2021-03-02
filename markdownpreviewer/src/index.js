684127aba4d4966837e0b2f77134dddb56a99170
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-02 11:58:10 -0500

    added bootstrap to the project

--- markdownpreviewer/src/index.js
+++ markdownpreviewer/src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 import marked from "marked";
-import { Tooltip, Toast, Popover } from "bootstrap";
 
 const initalState = `
 this is a paragraph
