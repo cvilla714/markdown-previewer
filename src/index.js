6a0682feede86d983386f47d4cddc4cf0bd8899d
Cosmel Villalobos <cosmelvg@gmail.com>
2021-03-02 12:47:19 -0500

    added boostrap to the project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "bootstrap/dist/css/bootstrap.css";
+// import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import marked from "marked";
 
 const initalState = `
